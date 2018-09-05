bd3230bfb12f82bc16a990d53a11596bb34aafc8
Mengyang Bai <mengyang.b@exciteholidays.com>
2018-09-05 15:53:10 +1000

    Koko Eating Banana

--- array/KoroEatingBanana.py
+++ array/KoroEatingBanana.py
@@ -6,12 +6,12 @@ class Solution:
         :rtype: int
         """
         # Binary Search
-        def isOK(K):  # 判断当前值，是否满足要求
+        def isOK(K):  
             return sum((p-1)//K + 1 for p in piles) <= H
 
-        low, high = 1, max(piles)  # 初始化上下界
+        low, high = 1, max(piles)  
 
-        while low < high:  # 以low ，high 为上下界，开始二分搜索
+        while low < high: 
             mid = (low + high) // 2
             if not isOK(mid):
                 low = mid + 1
