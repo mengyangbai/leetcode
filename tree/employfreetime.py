fd1e6edd94dc166d7bc1c998d2a7a7de5f9f2dda
Mengyang Bai <mengyang.b@exciteholidays.com>
2018-09-25 11:02:40 +1000

    Employ free time

--- tree/employfreetime.py
+++ tree/employfreetime.py
@@ -4,19 +4,20 @@ class Interval:
     def __init__(self, s=0, e=0):
         self.start = s
         self.end = e
-
-def employeeFreeTime(self, avails):
-    avails = list(sorted(itertools.chain(*avails), key=lambda interval: interval.start))
-    stack = [avails[0]]
-    res = list()
-    for cur in avails[1:]:
-        top = stack.pop()
-        if top.end < cur.start:
-            res.append(Interval(top.end, cur.start))
-            stack.append(cur)
-        else:
-            if cur.end <= top.end:
-                stack.append(top)
-            else:
+        
+class Solution:
+    def employeeFreeTime(self, avails):
+        avails = list(sorted(itertools.chain(*avails), key=lambda interval: interval.start))
+        stack = [avails[0]]
+        res = list()
+        for cur in avails[1:]:
+            top = stack.pop()
+            if top.end < cur.start:
+                res.append(Interval(top.end, cur.start))
                 stack.append(cur)
-    return res
+            else:
+                if cur.end <= top.end:
+                    stack.append(top)
+                else:
+                    stack.append(cur)
+        return res
