a7d7bf475cc565e6399f108deb35c9b8d4fcc465
Mengyang Bai <mengyang.b@exciteholidays.com>
2018-11-02 09:57:48 +1100

    maximized distance closet person

--- array/maximizedistancetoclosetperson.py
+++ array/maximizedistancetoclosetperson.py
@@ -4,4 +4,18 @@ class Solution:
         :type seats: List[int]
         :rtype: int
         """
-        
+        index = -200000
+        _len = len(seats)
+        ans = [0] * _len
+        for i in range(_len):
+            if seats[i] == 1:
+                index = i
+            else:
+                ans[i] = abs(i - index)
+        index = -200000
+        for i in range(_len - 1, -1, -1):
+            if seats[i] == 1:
+                index = i
+            else:
+                ans[i] = min(abs(i - index), ans[i])
+        return max(ans)
