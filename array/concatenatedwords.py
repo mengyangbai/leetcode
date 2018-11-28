f1dc8e19b7c5395f9c38c86dc9399bae76018bc3
Mengyang Bai <mengyang.b@exciteholidays.com>
2018-11-29 10:41:52 +1100

    concatenated word

---
+++ array/concatenatedwords.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findAllConcatenatedWordsInADict(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        ans = []
+        self.wordSet = set(words)
+        for word in words:
+            self.wordSet.remove(word)
+            if self.search(word):
+                ans.append(word)
+            self.wordSet.add(word)
+        return ans
+
+    def search(self, word):
+        if word in self.wordSet:
+            return True
+        for idx in range(1, len(word)):
+            if word[:idx] in self.wordSet and self.search(word[idx:]):
+                return True
+        return False
