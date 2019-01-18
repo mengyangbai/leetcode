6bf823070082f858e8191b124df7ca638da8ffdd
Mengyang Bai <mengyang.b@exciteholidays.com>
2019-01-18 16:53:42 +1100

    20190118

--- dfs/busroutes.py
+++ dfs/busroutes.py
@@ -37,5 +37,38 @@ class Solution(object):
         return 0 if S == T else ans if found else -1 
 
 
+class MySolution(object):
+    '''
+    TLE
+    ''' 
+    def numBusesToDestination(self, routes, S, T): 
+        """ 
+        :type routes: List[List[int]] 
+        :type S: int 
+        :type T: int 
+        :rtype: int 
+        """ 
+        res = []
+
+        def dfs(routes,S,T,path=[]):
+            path=path+[S]
+            for route in routes:
+                if S in route and T in route:
+                    path=path+[T]
+                    res.append(path)
+                    return
+                elif S in route and T not in route:
+                    for anostation in route:
+                        if anostation not in path:
+                            dfs(routes,anostation,T,path)
+
+        dfs(routes,S,T)
+
+        if len(res) == 0:
+            return -1
+        else:
+            return min(len(route) for route in res) - 1
 a = Solution()
-print(a.numBusesToDestination([[1,2,7],[3,6,7]],1,6))
+print(a.numBusesToDestination([[25,33],[3,5,13,22,23,29,37,45,49],[15,16,41,47],[5,11,17,23,33],[10,11,12,29,30,39,45],[2,5,23,24,33],[1,2,9,19,20,21,23,32,34,44],[7,18,23,24],[1,2,7,27,36,44],[7,14,33]],
+7,
+47))
