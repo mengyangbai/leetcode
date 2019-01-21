6cde445052664d7d85e314d5409383f96aedc519
Mengyang Bai <mengyang.b@exciteholidays.com>
2019-01-22 10:59:59 +1100

    20190122

---
+++ tree/bst/splitBst.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def splitBST(self, root, V):
+        """
+        :type root: TreeNode
+        :type V: int
+        :rtype: List[TreeNode]
+        """ 
+        if not root:
+            return [None,None]
+        
+        if root.val > V:
+            left,right = self.splitBST(root.left,V)
+            root.left = right
+            return [left,root]
+        else:
+            left,right = self.splitBST(root.right,V)
+            root.right = left
+            return [root,right]
