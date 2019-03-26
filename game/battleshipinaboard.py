9def6099572377c359d6d609725ce7e140c0468c
Mengyang Bai <mengyang.b@exciteholidays.com>
2019-03-27 10:21:26 +1100

    20190327

---
+++ game/battleshipinaboard.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def countBattleships(self, board):
+        if len(board) == 0: return 0
+        m, n = len(board), len(board[0])
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'X' and (i == 0 or board[i-1][j] == '.') and (j == 0 or board[i][j-1] == '.'):
+                    count += 1
+        return count
+
+'''
+    public int countBattleships(char[][] board) {
+        int m = board.length;
+        if (m==0) return 0;
+        int n = board[0].length;
+        
+        int count=0;
+        
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (board[i][j] == '.') continue;
+                if (i > 0 && board[i-1][j] == 'X') continue;
+                if (j > 0 && board[i][j-1] == 'X') continue;
+                count++;
+            }
+        }
+        
+        return count;
+    }
+'''
