099bf39dbfded1ab8e7596382cdf1fa4c606738a
Mengyang Bai <mengyang.b@exciteholidays.com>
2019-05-30 10:38:11 +1000

    20190530

---
+++ game/poisonedduration.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findPoisonedDuration(self, timeSeries, duration):
+        ans = duration * len(timeSeries)
+        for i in range(1,len(timeSeries)):
+            ans -= max(0, duration - (timeSeries[i] - timeSeries[i-1]))
+        return ans
+
+class BetterSolution:
+    def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
+        
+        time = 0
+        
+        last = 0
+        for i in timeSeries:
+            if i > last:
+                time += duration
+            else:
+                time += duration - (last - i)
+            last = i+duration
+        return time
